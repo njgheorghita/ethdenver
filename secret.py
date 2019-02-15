95beafd4b947c359b5da4963b9b339c8b3b31ebc
Nick Gheorghita <nickgheorghita@gmail.com>
2019-02-15 09:02:35 -0700

    Update scripts

--- secret.py
+++ secret.py
@@ -4,14 +4,13 @@ from web3.providers.auto import load_provider_from_uri
 from web3.middleware import construct_sign_and_send_raw_middleware
 from web3 import Web3
 
-
-DR_PK = '0x<insert private key here>'
+DR_PK = <insert_private_key_here>
 DR_ADDRESS = Account.privateKeyToAccount(DR_PK).address
 
-COOP_PK = '0x<insert private key here>'
+COOP_PK = <insert_private_key_here>
 COOP_ADDRESS = Account.privateKeyToAccount(COOP_PK).address
 
-GMA_PK = '0x<insert private key here>'
+GMA_PK = <insert_private_key_here>
 GMA_ADDRESS = Account.privateKeyToAccount(GMA_PK).address
 
 
@@ -23,7 +22,6 @@ coop_w3.enable_unstable_package_management_api()
 
 
 def print_balances(w3, multisig):
-    print("Dr: ", w3.eth.getBalance(DR_ADDRESS))
-    print("Grandma: ", w3.eth.getBalance(GMA_ADDRESS))
-    print("Multisig: ", w3.eth.getBalance(multisig))
-    print("Coop: ", w3.eth.getBalance(COOP_ADDRESS))
+    print(f"Grandma:  {w3.fromWei(w3.eth.getBalance(GMA_ADDRESS), 'ether')} ETH")
+    print(f"Multisig: {w3.fromWei(w3.eth.getBalance(multisig), 'ether')} ETH")
+    print(f"Coop:     {w3.fromWei(w3.eth.getBalance(COOP_ADDRESS), 'ether')} ETH")
